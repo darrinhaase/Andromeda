e47461d661c773f36ac8504e9abf984bce098ca2
DarrinHaase <darrin@darrintech.com>
2023-05-06 19:16:37 -0500

    Added  version detection to auto-updater

--- Updater/main.go
+++ Updater/main.go
@@ -5,15 +5,67 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"runtime"
+	"encoding/json"
 )
 
+type LatestData struct {
+	Release string `json:"release"`
+}
+
+type Data struct {
+	Latest LatestData `json:"latest"`
+	Current string `json:"current"`
+}
+
 func main() {
+	var latest string;
+	var current string;
+
+	fileContent, err := ioutil.ReadFile("version.dat")
+
+	var fileData Data
+	err = json.Unmarshal(fileContent, &fileData)
+	current = fileData.Current
+
+	resp, err := http.Get("https://f.techman.dev/d/andromeda/version.json")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body,err := ioutil.ReadAll(resp.Body)
+
+	var onlineData Data
+	err = json.Unmarshal(body, &onlineData)
+	latest = onlineData.Latest.Release
+
+	if (current != latest) {
+		update(latest)
+	}
+
+	var command string
+
+	if runtime.GOOS == "windows" {
+		command = "./Andromeda.exe"
+	} else {
+		command = "./Andromeda"
+	}
+
+	cmd := exec.Command(command)
+	cmd.Start()
+	os.Exit(0)
+}
+
+
+func update(newVersion string) {
 
 	var url string;
 
 	if runtime.GOOS == "windows" {
-		url = ""
+		url = "https://credo-downloads.s3.us-east-2.amazonaws.com/public/v1.011/Credo+Calculator+Installer.exe"
 	} else if runtime.GOOS == "linux" {
 		url = ""
 	} else if runtime.GOOS == "darwin" {
@@ -37,7 +89,15 @@ func main() {
 	}
 
 	// Write the response body to a file
-	file, err := os.Create("out.exe")
+	var fileName string
+
+	if runtime.GOOS == "windows" {
+		fileName = "Andromeda.exe"
+	} else {
+		fileName = "Andromeda"
+	}
+
+	file, err := os.Create(fileName)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -49,4 +109,6 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
+
+	ioutil.WriteFile("version.dat", []byte("{\"current\":\""+newVersion+"\"}"), 0644)
 }
